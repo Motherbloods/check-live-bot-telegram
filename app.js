b1a4ec2b07f24419eefa6dd68b5a007ac54687f2
Motherbloods <habibskh06@gmail.com>
2024-07-28 12:32:49 +0700

    a

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ module.exports = async (req, res) => {
       const isLive = await page.evaluate(() => {
         return !document.body.innerText.includes("LIVE has ended");
       });
-      console.log(`Is ${USERNAME_TIKTOK} live:`, isLive);
+      console.log(`Is ${USERNAME_TIKTOK} lives:`, isLive);
 
       if (isLive) {
         console.log(
